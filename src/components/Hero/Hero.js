7afe883a08e6e459c7103f1b9800e4d8cd37a301
Grzegorz Głuchowski <ggrzegorz89@gmail.com>
2021-08-14 14:40:26 +0200

    Init project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '/Hero.scss';
+
+const Hero = () => (
+  <header>
+    <h2>Things to do</h2>
+    <img>
+  </header>
+);
+
+export default Hero;
---
+++ src/components/List/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './List.scss';
+
+class List extends React.Component {
+  render() {
+    return(
+      <section className={styles.component}>
+        <h2>Lista rzeczy do wykonania</h2>
+
+      </section>
+    )
+  }
+}
+
+export default List;
